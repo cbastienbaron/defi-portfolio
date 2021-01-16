413b8eb0c7d558c314be7513e620120848529970
Kolibri1990 <defiportfoliomanagement@gmail.com>
2021-01-16 15:20:33 +0100

    Update process builder
    Change batch files to process builder.

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -6,4 +6,8 @@ public class Transaction {
     public String type;
     public String poolID;
     public String amounts[];
+
+    public Transaction() {
+
+    }
 }
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -4,6 +4,7 @@ import java.io.*;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
+import com.google.gson.GsonBuilder;
 
 public class test {
 
@@ -12,67 +13,77 @@ public class test {
 	    String strPathAppData = System.getenv("APPDATA")+ "\\defi-portfolio\\";
 	    String strFileLoadData = strPathAppData+ "\\loadData.bat";
 	    String strFileBlockCount = strPathAppData+ "\\loadData.bat";
-	    	
-	      try {
-	    	  
-//	    	  FileWriter myWriter = new FileWriter(strFileBlockCount);
-//	    	  myWriter.write("C:\\Users\\User\\Desktop\\defichain-1.3.17-x86_64-w64-mingw32\\defichain-1.3.17\\bin\\defi-cli.exe getblockcount > " +strPathAppData + "blockCount.txt");
-//	    	  myWriter.close();
-//
-//	          Runtime.getRuntime().exec(new String[] {"cmd","/k", "Start "+strFileBlockCount});
-//
-//
-//
-//	    	  FileReader f = new FileReader(strPathAppData + "blockCount.txt");
-//	    	  int c;
-//	    	  String strBlockCount="";
-//	    	  while ((c = f.read()) != -1) {
-//
-//	    		  strBlockCount = strBlockCount+(char)c;
-//	    		         }
-//	    	  strBlockCount = strBlockCount.replace("\n","");
-//	    	  strBlockCount = strBlockCount.replace("\r","");
-//	    	  strBlockCount = "100";
-//
-//	    	  int blockCount = Integer.parseInt(strBlockCount);
-//
-//	    	  myWriter = new FileWriter(strFileLoadData);
-//	          myWriter.write("C:\\Users\\User\\Desktop\\defichain-1.3.17-x86_64-w64-mingw32\\defichain-1.3.17\\bin\\defi-cli.exe listaccounthistory mine {\\\"limit\\\":"+strBlockCount+"} >" +strPathAppData+ "walletData.json");
-//	          myWriter.close();
-//
-//	          Runtime.getRuntime().exec(new String[] {"cmd","/k", "Start "+strFileLoadData});
+		Gson gson = new Gson();
+		int latestBlock;
+		Transaction[] transactions = new Transaction[0];
 
-			  Gson gson = new Gson();
-
-			  try (Reader reader = new FileReader(strPathAppData+ "walletData.json")) {
-
+		try (Reader reader = new FileReader(strPathAppData+ "portfolioData.json")) {
 				  // Convert JSON File to Java Object
-				  Transaction[] transactions = gson.fromJson(reader, Transaction[].class);
+			transactions = gson.fromJson(reader, Transaction[].class);
+			latestBlock = transactions[0].blockHeight;
 
-				  // print staff
-				  System.out.println(transactions);
+			String line;
+			Process p = Runtime.getRuntime().exec("C:\\Users\\User\\Desktop\\defichain-1.3.17-x86_64-w64-mingw32\\defichain-1.3.17\\bin\\defi-cli.exe getblockcount");
 
-			  } catch (IOException e) {
-				  e.printStackTrace();
-			  }
+			StringBuilder processOutput = new StringBuilder();
+
+			try (BufferedReader processOutputReader = new BufferedReader(
+					new InputStreamReader(p.getInputStream()));)
+			{
+				String readLine;
+
+				while ((readLine = processOutputReader.readLine()) != null)
+				{
+					processOutput.append(readLine + System.lineSeparator());
+				}
+
+				p.waitFor();
+			}
+
+			int blockCount = Integer.parseInt(processOutput.toString().trim());
+			int blockHeight = blockCount-latestBlock;
+
+
+			 p = Runtime.getRuntime().exec("C:\\Users\\User\\Desktop\\defichain-1.3.17-x86_64-w64-mingw32\\defichain-1.3.17\\bin\\defi-cli.exe listaccounthistory mine {\\\"depth\\\":"+blockHeight+100+",\\\"limit\\\":"+(blockHeight+10)*3+"}");
+
+			 processOutput = new StringBuilder();
+
+			try (BufferedReader processOutputReader = new BufferedReader(
+					new InputStreamReader(p.getInputStream()));)
+			{
+				String readLine;
+
+				while ((readLine = processOutputReader.readLine()) != null)
+				{
+					processOutput.append(readLine + System.lineSeparator());
+				}
+
+				p.waitFor();
+			}
+
+			String jsonTransaction = processOutput.toString().trim();
 
 
 
 
+			Transaction[] transactionsNew = gson.fromJson(jsonTransaction, Transaction[].class);
+
+			//Loop for Blockheight 
 
 
 
-			  //Transaction[] founderArray = gson.fromJson(jsonReader, Transaction[].class);
-				int a=2;
 
+		Gson gsonWriter = new GsonBuilder().setPrettyPrinting().create();
+			// Java objects to File
+			try (FileWriter writer = new FileWriter(strPathAppData+ "test.json")) {
+				gsonWriter.toJson(transactions, writer);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			} catch (IOException | InterruptedException e) {
+				e.printStackTrace();
+			}
 
-
-		  } catch (Exception e) {
-	  			e.printStackTrace();
-	  		}
-
-	
-		
 	}
 
 }
