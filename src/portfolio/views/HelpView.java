be03e830804a8f32b028603964f88455fc8eaf81
DeFi-PortfolioManagement <77515509+DeFi-PortfolioManagement@users.noreply.github.com>
2021-03-25 21:41:37 +0100

    Linux help links

--- src/portfolio/views/HelpView.java
+++ src/portfolio/views/HelpView.java
@@ -1,23 +1,14 @@
 package portfolio.views;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import portfolio.controllers.DonateController;
 import portfolio.controllers.HelpController;
 import portfolio.controllers.SettingsController;
 
 import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -50,33 +41,93 @@ public class HelpView implements Initializable {
 
 
     public void defichain() {
-        try {
-            Desktop.getDesktop().browse(new URL("https://defichain.com/").toURI());
-        } catch (IOException | URISyntaxException e) {
-            SettingsController.getInstance().logger.warning("Exception occured: " + e.toString());
+        if (SettingsController.getInstance().getPlatform().equals("linux")) {
+            // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't work on some Linux implementations
+            try {
+                if (Runtime.getRuntime().exec(new String[]{"which", "xdg-open"}).getInputStream().read() != -1) {
+                    Runtime.getRuntime().exec(new String[]{"xdg-open", "https://defichain.com/"});
+                } else {
+                    System.out.println("xdg-open is not supported!");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Desktop.getDesktop().browse(new URL("https://defichain.com/").toURI());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void defichainwiki() {
-        try {
-            Desktop.getDesktop().browse(new URL("https://defichain-wiki.com/wiki/DeFiChain-Portfolio").toURI());
-        } catch (IOException | URISyntaxException e) {
-            SettingsController.getInstance().logger.warning("Exception occured: " + e.toString());
+        if (SettingsController.getInstance().getPlatform().equals("linux")) {
+            // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't work on some Linux implementations
+            try {
+                if (Runtime.getRuntime().exec(new String[]{"which", "xdg-open"}).getInputStream().read() != -1) {
+                    Runtime.getRuntime().exec(new String[]{"xdg-open", "https://defichain-wiki.com/wiki/DeFiChain-Portfolio"});
+                } else {
+                    System.out.println("xdg-open is not supported!");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Desktop.getDesktop().browse(new URL("https://defichain-wiki.com/wiki/DeFiChain-Portfolio").toURI());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
         }
     }
     public void github() {
-        try {
-            Desktop.getDesktop().browse(new URL("https://github.com/DeFi-PortfolioManagement/defi-portfolio/blob/master/README.md").toURI());
-        } catch (IOException | URISyntaxException e) {
-            SettingsController.getInstance().logger.warning("Exception occured: " + e.toString());
+        if (SettingsController.getInstance().getPlatform().equals("linux")) {
+            // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't work on some Linux implementations
+            try {
+                if (Runtime.getRuntime().exec(new String[]{"which", "xdg-open"}).getInputStream().read() != -1) {
+                    Runtime.getRuntime().exec(new String[]{"xdg-open", "https://github.com/DeFi-PortfolioManagement/defi-portfolio/blob/master/README.md"});
+                } else {
+                    System.out.println("xdg-open is not supported!");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Desktop.getDesktop().browse(new URL("https://github.com/DeFi-PortfolioManagement/defi-portfolio/blob/master/README.md").toURI());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void btnTelegram()  {
-        try {
-            Desktop.getDesktop().browse(new URL("https://t.me/DeFiChainPortfolio").toURI());
-        } catch (IOException | URISyntaxException e) {
-            SettingsController.getInstance().logger.warning("Exception occured: " + e.toString());
+        if (SettingsController.getInstance().getPlatform().equals("linux")) {
+            // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't work on some Linux implementations
+            try {
+                if (Runtime.getRuntime().exec(new String[]{"which", "xdg-open"}).getInputStream().read() != -1) {
+                    Runtime.getRuntime().exec(new String[]{"xdg-open", "https://t.me/DeFiChainPortfolio"});
+                } else {
+                    System.out.println("xdg-open is not supported!");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Desktop.getDesktop().browse(new URL("https://t.me/DeFiChainPortfolio").toURI());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -88,10 +139,25 @@ public class HelpView implements Initializable {
     }
 
     public void defichainexplained() {
-        try {
-            Desktop.getDesktop().browse(new URL("https://defichain-explained.com/").toURI());
-        } catch (IOException | URISyntaxException e) {
-            SettingsController.getInstance().logger.warning("Exception occured: " + e.toString());
+        if (SettingsController.getInstance().getPlatform().equals("linux")) {
+            // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't work on some Linux implementations
+            try {
+                if (Runtime.getRuntime().exec(new String[]{"which", "xdg-open"}).getInputStream().read() != -1) {
+                    Runtime.getRuntime().exec(new String[]{"xdg-open", "https://defichain-explained.com/"});
+                } else {
+                    System.out.println("xdg-open is not supported!");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Desktop.getDesktop().browse(new URL("https://defichain-explained.com/").toURI());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
